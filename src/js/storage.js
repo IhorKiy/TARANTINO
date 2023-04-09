daff005a000b1372ae80b91cc145ebe1f78ea011
Олена Пестова <nowalena9@gmail.com>
2023-04-09 22:51:47 +0300

    3 фунуції додала в ексорт 6 забрала в модалку які до неї відносяться

--- src/js/storage.js
+++ src/js/storage.js
@@ -94,68 +94,15 @@ function loadCurrentPage() {
     console.log("from loadCurrentPage",arrayMovies)
     return arrayMovies;
 }
-//******************** mylibrary *************************
-//перевіряємо чи є в списку WATCHED міняємо напис на кнопці
-function isWatched(movie,btn){
-    const movies = load(WATCHED_KEY);
-    if (!movies.includes(movie)) {
-         btn.textContent = "ADD to WATCHED";  //чи додавати відповідний клас
-    }
-     else btn.textContent = "REMOVE FROM WATCHED"; //чи додавати відповідний клас
-}
-//перевіряємо чи є в списку QUEUE міняємо напис на кнопці
-function isQueue(movie,btn) {
-    const movies = load(QUEUE_KEY);
-    if (!movies.includes(movie)) {
-         btn.textContent = "ADD to QUEUE"; //чи додавати відповідний клас
-    }
-     else btn.textContent = "REMOVE FROM QUEUE"; //чи додавати відповідний клас
-}
-//const посилання на кнопку = document.querySelector("click", addToWatched)
-//при натисненні ADD TO WATCHED додаємо movie в localStorage якщо там його нема
-function addToWatched(movie,btn) { //btn це ref на кнопку в модалці фільма,  фільма,яка відповідає за його ознаку WATChED
-    const movies = load(WATCHED_KEY);
-    if (!movies.includes(movie) && btn.textContent === "ADD TO WATCHED") { //
-        movies.push(movie);
-        save(WATCHED_KEY,movies);
-    } else console.log('Цей фільм вже є в watched або кнопка не add!');
-}
-//const посилання на кнопку = document.querySelector("click", removeFromWatched) це посилання на addToWatched але перевіряєемо textContent кнопки чи там REMOVE FROM WATCHED
-//при натисненні REMOVE FROM WATCHED видаляємо з localStorage якщо він там  є
-function removeFromWatched (movie,btn){ //btn це ref на кнопку в модалці  фільма,яка відповідає за його ознаку WATChED
-    const movies = load(WATCHED_KEY);
-    if (movies.includes(movie) && btn.textContent === "REMOVE FROM WATCHED") {
-        movies = movies.filter(({ id }) => id !== movie.id)
-        save(WATCHED_KEY, movies);
-    } else console.log('Цього фільма нема в watched або кнопка не remove!');
-}
+
 // при натисненні на кнопку WATCHED 
-//const посилання на кнопку WATCHED = document.querySelector("click", loadFromWatched)
 //витягаємо  масив фільмів для рендерінга з localStorage(watched)
 function loadFromWatched() {
     const movies = load(WATCHED_KEY);
     return movies;
 }
-//const посилання на кнопку = document.querySelector("click", addToQueue)
-//при натисненні ADD TO QUEUE додаємо в localStorage якщо там його нема
-function addToQueue(movie,btn) { //btn це ref на кнопку в модалці  фільма,яка відповідає за його ознаку QUEUE 
-    const movies = load(QUEUE_KEY);
-    if (!movies.includes(movie) && btn.textContent === "ADD TO QUEUR") {
-        movies.push(movie);
-        save(QUEUE_KEY,movies);
-    } else console.log('Цей фільм вже є в queue або кнопка не add!');
-}
-//const посилання на кнопку = document.querySelector("click", removeFromQueue) то саме що ADD TO QUEUE
-//при натисненні REMOVE FROM QUEUE видаляємо з localStorage якщо він там  є перевіряєемо textContent кнопки чи там REMOVE FROM QUEUE
-function removeFromQueue (movie,btn){ //btn це ref на кнопку в модалці фільма, яка відповідає за його ознаку QUEUE
-    const movies = load(QUEUE_KEY);
-    if (movies.includes(movie) &&  btn.textContent=== "REMOVE FROM QUEUE") {
-        movies = movies.filter(({ id }) => id !== movie.id)
-        save(QUEUE_KEY, movies);
-    } else console.log('Цього фільма нема в queue або кнопка не remove!');
-}
-///при натисненні кнопки QUEUE витягаємо (треба поилання на кнопку)
-//const посилання на кнопку  QUEUE = document.querySelector("click", loadFromQueue)
+
+//при натисненні кнопки QUEUE витягаємо (треба поилання на кнопку)
 //витягаємо  масив фільмів для рендерінга з localStorage(queue)
 function loadFromQueue() {
     const movies = load(QUEUE_KEY);
@@ -174,13 +121,7 @@ export default {
   loadTrendMovies, //storage.loadTrendMovies() для рендерігна  поверне масив трендових фільмів  зі сховища 
   saveCurrentPage, //storage.saveCurrentPage(data)  той хто фетчить той має додати масив фільмів(data) в сховище 
   loadCurrentPage, //storage.loadCurrentPage() хто рендерить текучу сторінку ,той бере масив фільмів зі сховища
-  isWatched, //storage.isWatched(movie) перевіряє чи е в списку WATCHED і міняє напис на кпоці ADD TO  WATCHED/REMOVE FROM WATCHED 
-  isQueue,  //storage.isQueue(movie)еревіряє чи е в списку QUEUE і міняє напис на кпоці ADD TO QUEUE/REMOVE FROM QUEUE
-  addToWatched,  //storage.addToWatched(movie) при натисненні на ADD TO WATCHED  добавляє movie до сховища 
-  removeFromWatched, //storage.removeFromWatched(movie) при натисненні на REMOVE FROM WATCHED видаляє movie зі сховища 
   loadFromWatched,   //storage.loadFromWatched() коли натиснули WATCHED використовуємо для рендера сторінки в MyLibrary 
-  addToQueue,     //storage.addToQueue(movie) при натисненні на ADD TO QUEUE  добавляє movie до сховища 
-  removeFromQueue,  //storage.removeFromQueue(movie) при натисненні на REMOVE FROM QUEUE видаляє movie зі сховища 
   loadFromQueue,   //storage.loadFromQueue() коли натиснули QUEUE використовуємо для рендера сторінки в MyLibrary 
 };
 
