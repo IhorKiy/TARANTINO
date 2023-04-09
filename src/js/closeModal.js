eace82ef9c0206b02fb1ded076efebf7ab2d57d4
OleksandrVaniuta <vaniutasa@gmail.com>
2023-04-09 12:23:20 +0300

    consoled closeModal

--- src/js/closeModal.js
+++ src/js/closeModal.js
@@ -1,26 +1,26 @@
-const overlay = document.querySelector('.overlay');
-const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const modal = document.querySelector('.modal');
 
-window.addEventListener('keydown', e => {
-  if (modal.classList.contains('active')) {
-    handleKeyDown(e);
-  }
-});
+// window.addEventListener('keydown', e => {
+//   if (modal.classList.contains('active')) {
+//     handleKeyDown(e);
+//   }
+// });
 
-overlay.addEventListener('click', e => {
-  handleClick(e);
-});
+// overlay.addEventListener('click', e => {
+//   handleClick(e);
+// });
 
-const handleKeyDown = e => {
-  if (e.code === 'Escape') {
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-  }
-};
+// const handleKeyDown = e => {
+//   if (e.code === 'Escape') {
+//     modal.classList.remove('active');
+//     overlay.classList.remove('active');
+//   }
+// };
 
-const handleClick = e => {
-  if (e.currentTarget == e.target) {
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-  }
-};
+// const handleClick = e => {
+//   if (e.currentTarget == e.target) {
+//     modal.classList.remove('active');
+//     overlay.classList.remove('active');
+//   }
+// };
