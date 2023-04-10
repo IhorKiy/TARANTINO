d889aa3f3cf40bb2b89adfdb3418c726981a7dd4
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-04-10 09:38:07 +0300

    some styles

--- src/js/getGenresNames.js
+++ src/js/getGenresNames.js
@@ -1,8 +1,11 @@
 
 export const getGenresNames = ids => {
     const savedGenres = JSON.parse(window.localStorage.getItem('genres'));
-    const genresNames = Object.entries(savedGenres)
+    let genresNames = Object.entries(savedGenres)
       .filter(([key]) => ids.includes(parseInt(key)))
       .map(([_, value]) => value);
+      if(genresNames.length > 3) {
+        return genresNames = [genresNames[0], genresNames[1], 'Other']
+       }
     return genresNames;
   };
