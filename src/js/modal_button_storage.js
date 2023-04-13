2af443be225c13d21ae01cf163c0a0fb9c55ffd3
Olena Pestova <nowalena9@gmail.com>
2023-04-13 09:29:33 +0300

    added isWatched/isQueue else{}

--- src/js/modal_button_storage.js
+++ src/js/modal_button_storage.js
@@ -20,17 +20,13 @@ console.log("refs.addToQueueBtn ", refs.addToQueueBtn );
 export function isWatched(movie,btn){
     const movies = storage.load(WATCHED_KEY) ||  [];
     if (!movies.includes(movie)) {
-<<<<<<< HEAD
         btn.textContent = addWatched;
         return true;
     }
-    else btn.textContent = removeWatched; 
-    return false;
-=======
-          btn.textContent = addWatched;
+    else {
+        btn.textContent = removeWatched;
+        return false;
     }
-     else btn.textContent = removeWatched; 
->>>>>>> f6183c501063320e27d1e9ee39aefc394c45db1c
 }
 //перевіряє чи Є цей movie в сховищі QUEUE і дає кнопці відповіний напис?
 export function isQueue(movie,btn) {
@@ -39,8 +35,10 @@ export function isQueue(movie,btn) {
         btn.textContent = addQueue; //чи додавати відповідний клас?
         return true;
     }
-    else btn.textContent = removeQueue; //чи додавати відповідний клас?
-    return false;
+    else {
+        btn.textContent = removeQueue; //чи додавати відповідний клас?
+        return false;
+    }
 }
 export { isWatched, isQueue };
 //при відкритті модалки перевіряємо чи movie Є в сховищі по
