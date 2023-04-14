f60ffbdf64e9791478f2ec3673a622e295d914e3
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-04-14 13:47:49 +0300

    service, searchquery, styles update

--- src/js/searchQuery.js
+++ src/js/searchQuery.js
@@ -1,44 +1,99 @@
-import ApiMovie, { apiMovie } from './serviseAPI';
+import { apiMovie } from './serviseAPI';
 import insertCardMarkup from './cardMarkup';
+import storage from './storage';
 import { Notify } from 'notiflix';
 import Loader from './loader';
+import Paginator from './paginator';
 
 const loader = new Loader();
-
+let userValue = '';
 const form = document.querySelector('.header__form');
 const input = form.querySelector(`input`);
 const list = document.querySelector(`.home__container`);
-// const list = document.querySelector(`.card__container`);
 const slider = document.querySelector(`.top-slider__section`);
+const paginatorQuery = new Paginator();
 
 form.addEventListener(`submit`, onSearch);
+
+function getQueryData(e) {
+  console.log('here');
+  let nowPage = paginatorQuery.getNumber(e);
+  if (nowPage) {
+    async function fetchByQueryFropmPag(value, page) {
+      try {
+        const res = await apiMovie.searchMovieByQuery(value, page);
+        console.log(page);
+
+        if (res.results.length === 0) {
+          loader.disable();
+          return Notify.failure(`Sorry, no movies were found for your search.`);
+        }
+        storage.saveCurrentPage(res.results);
+        storage.savePage(page);
+        insertCardMarkup(res.results, list);
+        ifPosterOfMovieIsNotFound();
+
+        loader.disable();
+        paginatorQuery.makeMarkup();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchByQueryFropmPag(userValue, paginatorQuery.currentPage);
+  }
+}
 
 function onSearch(e) {
   e.preventDefault();
 
-  const value = e.currentTarget.elements.searchQuery.value.trim();
-  apiMovie.query = value;
+  userValue = e.currentTarget.elements.searchQuery.value.trim();
 
-  if (input.value === '') {
+  if (userValue === '') {
     return Notify.info(`The input field cannot be empty!`);
+    return;
   }
 
   loader.enable();
-  fetchMovie();
+
+  fetchByQuery(userValue, 1);
+
+  paginatorQuery.pagination.addEventListener('click', getQueryData);
+
+  input.addEventListener('change', e => {
+    console.log(e.target.value);
+    if (e.target.value !== '') {
+      return;
+    }
+    window.location.href = './index.html';
+  });
 }
 
-function fetchMovie(e) {
-  apiMovie.searchMovieByQuery().then(data => {
-    if (data.results.length === 0) {
+async function fetchByQuery(value, page) {
+  try {
+    const res = await apiMovie.searchMovieByQuery(value, page);
+
+    if (res.results.length === 0) {
       loader.disable();
-      return Notify.failure(`Sorry, no movies were found for your search.`);
+      console.log(`Sorry, no movies were found for your search.`);
     }
 
+    paginatorQuery.totalPages = res.total_pages;
+    storage.saveCurrentPage(res.results);
+    storage.savePage(page);
+
     slider.style.display = `none`;
-    insertCardMarkup(data.results, list);
+
+    insertCardMarkup(res.results, list);
     ifPosterOfMovieIsNotFound();
+
     loader.disable();
-  });
+    paginatorQuery.pagination.innerHTML = '';
+
+    paginatorQuery.makeMarkup();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // if poster of film is not found
@@ -51,3 +106,4 @@ function ifPosterOfMovieIsNotFound() {
     }
   });
 }
+
